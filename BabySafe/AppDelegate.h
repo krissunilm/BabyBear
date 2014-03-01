7d87ed8a1e68b08af29b68746d8466886df12514
Kris Maganti <krissunilm@yahoo.com>
2014-03-01 10:33:36 -0800

    Initial Commit

---
+++ BabySafe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BabySafe
+//
+//  Created by sunil maganti on 3/1/14.
+//  Copyright (c) 2014 Kvana Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BabySafe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BabySafe
+//
+//  Created by sunil maganti on 3/1/14.
+//  Copyright (c) 2014 Kvana Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
